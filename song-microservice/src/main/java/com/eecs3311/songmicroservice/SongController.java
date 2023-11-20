72960c5f2615e78e8e4da25f9142b9ad42beb874
Aayush9029 <developer@aayush.art>
2023-11-20 04:07:39 -0500

    Added Documentation for SongController.java + Update Functions
    Making it easier for peers to run and debug

--- song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
+++ song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
@@ -3,7 +3,6 @@ package com.eecs3311.songmicroservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,70 +26,93 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping
 public class SongController {
 
-	@Autowired
-	private final SongDal songDal;
+    @Autowired
+    private final SongDal songDal;
 
-	private OkHttpClient client = new OkHttpClient();
+    private OkHttpClient client = new OkHttpClient();
 
-	public SongController(SongDal songDal) {
-		this.songDal = songDal;
-	}
+    public SongController(SongDal songDal) {
+        this.songDal = songDal;
+    }
 
-	/**
-	 * This method is partially implemented for you to follow as an example of
-	 * how to complete the implementations of methods in the controller classes.
-	 * 
-	 * @param songId
-	 * @param request
-	 * @return
-	 */
-	@GetMapping("/getSongById/{songId}")
-	public ResponseEntity<Map<String, Object>> getSongById(@PathVariable String songId, HttpServletRequest request) {
-		System.out.println("🧑‍💻 getSongById called with songId: " + songId);
-		Map<String, Object> response = new HashMap<>();
-		response.put("path", Utils.getUrl(request));
+    /**
+     * This method is partially implemented for you to follow as an example of
+     * how to complete the implementations of methods in the controller classes.
+     *
+     * @param songId
+     * @param request
+     * @return
+     */
 
-		DbQueryStatus dbQueryStatus = songDal.findSongById(songId);
-		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
-	}
+    /*
+     * Usage: /getSongById/ID -> returns a JSON object with the song with the given
+     */
+    @RequestMapping(value = "/getSongById/{songId}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> getSongById(@PathVariable String songId, HttpServletRequest request) {
+        System.out.println("🧑‍💻 getSongById called with songId: " + songId);
 
-	@GetMapping("/getSongTitleById/{songId}")
-	public ResponseEntity<Map<String, Object>> getSongTitleById(@PathVariable String songId,
-			HttpServletRequest request) {
-		Map<String, Object> response = new HashMap<>();
-		response.put("path", Utils.getUrl(request));
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("path", String.format("GET %s", Utils.getUrl(request)));
 
-		DbQueryStatus dbQueryStatus = songDal.getSongTitleById(songId);
-		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
-	}
+        DbQueryStatus dbQueryStatus = songDal.findSongById(songId);
+        response.put("message", dbQueryStatus.getMessage());
+        return Utils.setResponseStatus(response,
+                dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 
-	@RequestMapping(value = "/deleteSongById/{songId}", method = RequestMethod.DELETE)
-	public ResponseEntity<Map<String, Object>> deleteSongById(@PathVariable String songId, HttpServletRequest request) {
-		Map<String, Object> response = new HashMap<>();
-		response.put("path", Utils.getUrl(request));
+    }
 
-		DbQueryStatus dbQueryStatus = songDal.deleteSongById(songId);
-		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
-	}
+    /*
+     * Usage: /getSongTitleById/ID -> returns a JSON object with the title of the
+     */
 
-	@RequestMapping(value = "/addSong", method = RequestMethod.POST)
-	public ResponseEntity<Map<String, Object>> addSong(@RequestBody Song song, HttpServletRequest request) {
-		Map<String, Object> response = new HashMap<>();
-		response.put("path", Utils.getUrl(request));
+    @RequestMapping(value = "/getSongTitleById/{songId}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> getSongTitleById(@PathVariable String songId,
+            HttpServletRequest request) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("path", String.format("GET %s", Utils.getUrl(request)));
 
-		DbQueryStatus dbQueryStatus = songDal.addSong(song);
-		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
-	}
+        DbQueryStatus dbQueryStatus = songDal.getSongTitleById(songId);
+        return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
+    }
 
-	@RequestMapping(value = "/updateSongFavouritesCount", method = RequestMethod.PUT)
-	public ResponseEntity<Map<String, Object>> updateFavouritesCount(@RequestBody Map<String, String> params,
-			HttpServletRequest request) {
-		Map<String, Object> response = new HashMap<>();
-		response.put("path", Utils.getUrl(request));
+    /*
+     * Usage: /deleteSongById/ID -> deletes the song with the given ID
+     */
+    @RequestMapping(value = "/deleteSongById/{songId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Map<String, Object>> deleteSongById(@PathVariable String songId, HttpServletRequest request) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("path", String.format("DELETE %s", Utils.getUrl(request)));
 
-		String songId = params.get("songId");
-		boolean shouldDecrement = Boolean.parseBoolean(params.get("shouldDecrement"));
-		DbQueryStatus dbQueryStatus = songDal.updateSongFavouritesCount(songId, shouldDecrement);
-		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
-	}
+        DbQueryStatus dbQueryStatus = songDal.deleteSongById(songId);
+        return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
+    }
+
+    /*
+     * Usage: /addSong -> adds the song in the request body to the database
+     */
+    @RequestMapping(value = "/addSong", method = RequestMethod.POST)
+    public ResponseEntity<Map<String, Object>> addSong(@RequestBody Song song, HttpServletRequest request) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("path", String.format("POST %s", Utils.getUrl(request)));
+
+        DbQueryStatus dbQueryStatus = songDal.addSong(song);
+        return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
+    }
+
+    /*
+     * Usage: /updateSongFavouritesCount?songId=ID&shouldDecrement=false -> updates
+     * the favourites count of the song with the given ID
+     */
+    //
+    @RequestMapping(value = "/updateSongFavouritesCount", method = RequestMethod.PUT)
+    public ResponseEntity<Map<String, Object>> updateFavouritesCount(@RequestBody Map<String, String> params,
+            HttpServletRequest request) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("data", String.format("PUT %s", Utils.getUrl(request)));
+
+        String songId = params.get("songId");
+        boolean shouldDecrement = Boolean.parseBoolean(params.get("shouldDecrement"));
+        DbQueryStatus dbQueryStatus = songDal.updateSongFavouritesCount(songId, shouldDecrement);
+        return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
+    }
 }
