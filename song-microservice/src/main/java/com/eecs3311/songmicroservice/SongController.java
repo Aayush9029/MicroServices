2782c30e5e32505cf821cc48099de5928dc7a87b
Aayush9029 <developer@aayush.art>
2023-11-20 03:04:07 -0500

    CRUD for SongMicroServices Added "Implement CRUD endpoints in SongController for song management"
    - Add getSongById endpoint to retrieve song details by ID
    - Add getSongTitleById endpoint for fetching song title using ID
    - Add deleteSongById endpoint to enable song deletion by ID
    - Add addSong endpoint to support adding new songs to the database
    - Add updateFavouritesCount endpoint for updating song favourites count

--- song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
+++ song-microservice/src/main/java/com/eecs3311/songmicroservice/SongController.java
@@ -3,6 +3,7 @@ package com.eecs3311.songmicroservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,6 @@ public class SongController {
 
 	private OkHttpClient client = new OkHttpClient();
 
-	
 	public SongController(SongDal songDal) {
 		this.songDal = songDal;
 	}
@@ -39,70 +39,58 @@ public class SongController {
 	/**
 	 * This method is partially implemented for you to follow as an example of
 	 * how to complete the implementations of methods in the controller classes.
+	 * 
 	 * @param songId
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping(value = "/getSongById/{songId}", method = RequestMethod.GET)
-	public ResponseEntity<Map<String, Object>> getSongById(@PathVariable("songId") String songId,
-			HttpServletRequest request) {
-
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("path", String.format("GET %s", Utils.getUrl(request)));
+	@GetMapping("/getSongById/{songId}")
+	public ResponseEntity<Map<String, Object>> getSongById(@PathVariable String songId, HttpServletRequest request) {
+		System.out.println("🧑‍💻 getSongById called with songId: " + songId);
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", Utils.getUrl(request));
 
 		DbQueryStatus dbQueryStatus = songDal.findSongById(songId);
-
-		// TODO: uncomment these two lines when you have completed the implementation of findSongById in SongDal
-		// response.put("message", dbQueryStatus.getMessage());
-		// return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
-		
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: remove when the above 2 lines are uncommented
+		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 	}
 
-	
-	@RequestMapping(value = "/getSongTitleById/{songId}", method = RequestMethod.GET)
-	public ResponseEntity<Map<String, Object>> getSongTitleById(@PathVariable("songId") String songId,
+	@GetMapping("/getSongTitleById/{songId}")
+	public ResponseEntity<Map<String, Object>> getSongTitleById(@PathVariable String songId,
 			HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", Utils.getUrl(request));
 
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("path", String.format("GET %s", Utils.getUrl(request)));
-		// TODO: add any other values to the map following the example in getSongById
-
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: replace with return statement similar to in getSongById
+		DbQueryStatus dbQueryStatus = songDal.getSongTitleById(songId);
+		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 	}
 
-	
 	@RequestMapping(value = "/deleteSongById/{songId}", method = RequestMethod.DELETE)
-	public ResponseEntity<Map<String, Object>> deleteSongById(@PathVariable("songId") String songId,
-			HttpServletRequest request) {
+	public ResponseEntity<Map<String, Object>> deleteSongById(@PathVariable String songId, HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", Utils.getUrl(request));
 
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("path", String.format("DELETE %s", Utils.getUrl(request)));
-		// TODO: add any other values to the map following the example in getSongById
-
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: replace with return statement similar to in getSongById
+		DbQueryStatus dbQueryStatus = songDal.deleteSongById(songId);
+		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
 	}
 
-	
 	@RequestMapping(value = "/addSong", method = RequestMethod.POST)
-	public ResponseEntity<Map<String, Object>> addSong(@RequestBody Map<String, String> params,
-			HttpServletRequest request) {
+	public ResponseEntity<Map<String, Object>> addSong(@RequestBody Song song, HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", Utils.getUrl(request));
 
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("path", String.format("POST %s", Utils.getUrl(request)));
-		// TODO: add any other values to the map following the example in getSongById
-
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: replace with return statement similar to in getSongById
+		DbQueryStatus dbQueryStatus = songDal.addSong(song);
+		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 	}
 
-	
 	@RequestMapping(value = "/updateSongFavouritesCount", method = RequestMethod.PUT)
-	public ResponseEntity<Map<String, Object>> updateFavouritesCount(@RequestBody Map<String, String> params, HttpServletRequest request) {
+	public ResponseEntity<Map<String, Object>> updateFavouritesCount(@RequestBody Map<String, String> params,
+			HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("path", Utils.getUrl(request));
 
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("data", String.format("PUT %s", Utils.getUrl(request)));
-		// TODO: add any other values to the map following the example in getSongById
-
-		return ResponseEntity.status(HttpStatus.OK).body(response); // TODO: replace with return statement similar to in getSongById
+		String songId = params.get("songId");
+		boolean shouldDecrement = Boolean.parseBoolean(params.get("shouldDecrement"));
+		DbQueryStatus dbQueryStatus = songDal.updateSongFavouritesCount(songId, shouldDecrement);
+		return Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), null);
 	}
 }
--- song-microservice/src/main/java/com/eecs3311/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/eecs3311/songmicroservice/SongDalImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,31 +20,72 @@ public class SongDalImpl implements SongDal {
 
 	@Override
 	public DbQueryStatus addSong(Song songToAdd) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			db.insert(songToAdd);
+			return new DbQueryStatus("Song added successfully", DbQueryExecResult.QUERY_OK);
+		} catch (Exception e) {
+			return new DbQueryStatus("Error adding song: " + e.getMessage(), DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
 	}
 
 	@Override
 	public DbQueryStatus findSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Song song = db.findById(new ObjectId(songId), Song.class);
+			if (song == null) {
+				return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			}
+			DbQueryStatus status = new DbQueryStatus("Song found", DbQueryExecResult.QUERY_OK);
+			status.setData(song);
+			return status;
+		} catch (Exception e) {
+			return new DbQueryStatus("Error finding song: " + e.getMessage(), DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
 	}
 
 	@Override
 	public DbQueryStatus getSongTitleById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Song song = db.findById(new ObjectId(songId), Song.class);
+			if (song == null) {
+				return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			}
+			DbQueryStatus status = new DbQueryStatus("Song title found", DbQueryExecResult.QUERY_OK);
+			status.setData(song.getSongName());
+			return status;
+		} catch (Exception e) {
+			return new DbQueryStatus("Error finding song title: " + e.getMessage(),
+					DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
 	}
 
 	@Override
 	public DbQueryStatus deleteSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Query query = new Query(Criteria.where("_id").is(new ObjectId(songId)));
+			db.remove(query, Song.class);
+			return new DbQueryStatus("Song deleted successfully", DbQueryExecResult.QUERY_OK);
+		} catch (Exception e) {
+			return new DbQueryStatus("Error deleting song: " + e.getMessage(), DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
 	}
 
 	@Override
 	public DbQueryStatus updateSongFavouritesCount(String songId, boolean shouldDecrement) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Query query = new Query(Criteria.where("_id").is(new ObjectId(songId)));
+			Update update = new Update();
+			if (shouldDecrement) {
+				update.inc("songAmountFavourites", -1);
+			} else {
+				update.inc("songAmountFavourites", 1);
+			}
+			db.findAndModify(query, update, Song.class);
+			return new DbQueryStatus("Song favourites count updated successfully", DbQueryExecResult.QUERY_OK);
+		} catch (Exception e) {
+			return new DbQueryStatus("Error updating song favourites count: " + e.getMessage(),
+					DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
 	}
+
 }
